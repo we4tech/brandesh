aff07399542ef7b5106097634707bae34a619ea2
we4tech <hasan83bd@gmail.com>
2012-04-17 16:41:40 +0600

    added initial rails stack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandesh::Application.config.secret_token = '9f504e85748af3eb642c9ddbe099b17a06632db9b759f0d9269d5330af43839803c07ec42f8d1657c33dbee7b9ba128259604812f67cbbe06b27b4c35dfcb8be'
